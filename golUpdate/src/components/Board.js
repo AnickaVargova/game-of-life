396f5c690bde0eb362a4ef958f5afce27bb15a31
Anna Vargová <64069511+AnickaVargova@users.noreply.github.com>
2021-01-20 17:45:07 +0100

    update Board.js

--- golUpdate/src/components/Board.js
+++ golUpdate/src/components/Board.js
@@ -17,7 +17,7 @@ const Board = ({ board, handleClick, isRunning, tempo, step }) => {
         clearInterval(interval);
       }
     };
-  }, [isRunning, tempo]);
+  }, [isRunning, tempo,step]);
   return (
     <Table>
       <tbody>
